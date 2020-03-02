edf4dc5962afe366cbd0d2d9c962e11ef72526e8
Max Karpushonok <max.karpushonok@gmail.com>
2020-03-03 00:50:22 +0300

    Added addTasks method

--- AmoCRM.php
+++ AmoCRM.php
@@ -1,22 +1,40 @@
 <?php
+
+    /**
+     * Class AmoCRM
+     * Retrieving and adding data to amoCRM using the API
+     */
     class AmoCRM {
         private $user = null;
         private $domain = null;
         private $curl = null;
 
+        /**
+         * API page list
+         */
         const PAGES = array(
-            'AUTH' => '/private/api/auth.php?type=json',
+            'AUTH' => '/private/api/auth.php?type=json&',
             'LEADS' => '/api/v2/leads?',
             'TASKS' => '/api/v2/tasks?'
         );
 
+        /**
+         * AmoCRM constructor.
+         * @param String $login amocrm user login (e-mail)
+         * @param String $hash amocrm user hash (from profile)
+         * @param String $subDomain amocrm subdomain name
+         */
         public function __construct(String $login, String $hash, String $subDomain) {
             $this->user = array('USER_LOGIN' => $login, 'USER_HASH' => $hash);
             $this->domain = 'https://'.$subDomain.'.amocrm.ru';
             $this->curl = curl_init();
+
             $this->setOpt();
         }
 
+        /**
+         * @return mixed (associative array with authorized user data)
+         */
         public function authorization() {
             curl_setopt($this->curl, CURLOPT_URL, $this->domain.self::PAGES['AUTH']);
             curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, 'POST');
@@ -26,18 +44,41 @@
             return (json_decode($out, true))['response'];
         }
 
+        /**
+         * @param array|null $params (GET params (key=>value) for amoCRM request to leads)
+         * @return mixed (associative array with leads data)
+         */
         public function getLeads(array $params = null) {
             return $this->getPage(self::PAGES['LEADS'], $params);
         }
 
+        /**
+         * @param array|null $params (GET params (key=>value) for amoCRM request to tasks)
+         * @return mixed (associative array with tasks data)
+         */
         public function getTasks(array $params = null) {
             return $this->getPage(self::PAGES['TASKS'], $params);
         }
 
-        public function addTasks() {
-
+        /**
+         * @param array|null $params (POST params with associative array tasks data)
+         * @return mixed
+         */
+        public function addTasks(array $params = null) {
+            $tasks['add'] = $params;
+            curl_setopt($this->curl, CURLOPT_URL, $this->domain.self::PAGES['TASKS']);
+            curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, 'POST');
+            curl_setopt($this->curl, CURLOPT_POSTFIELDS, json_encode($tasks));
+            $out = curl_exec($this->curl);
+            $code = (int) curl_getinfo($this->curl, CURLINFO_HTTP_CODE);
+            $this->checkCode($code);
+            echo var_dump($out).'<br>'; //TODO delete
+            return true;
         }
 
+        /**
+         * Sets the default params for curl
+         */
         private function setOpt() {
             curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($this->curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
@@ -50,6 +91,11 @@
             curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST, 0);
         }
 
+        /**
+         * @param String $page (page url)
+         * @param array|null $params (get params (key=>value) for amoCRM request)
+         * @return mixed (associative array)
+         */
         private function getPage(String $page, array $params = null) {
             if ($params != null)
                 $params = implode('&', $params);
@@ -62,11 +108,18 @@
             return (json_decode($out, true))['_embedded']['items'];
         }
 
+        /**
+         * Close curl
+         */
         public function closeCurl() {
             if ($this->curl != null)
                 curl_close($this->curl);
         }
 
+        /**
+         * Checking code and display error if exists
+         * @param $code (error code number)
+         */
         private function checkCode($code) {
             $errors = array(
                 301 => 'Moved permanently',
@@ -80,12 +133,12 @@
             );
 
             try {
-                //Если код ответа не равен 200 или 204 - возвращаем сообщение об ошибке
+                //If answer code is not 200 or 204 - return error message
                 if ($code != 200 && $code != 204) {
                     throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undescribed error', $code);
                 }
             } catch (Exception $E) {
-                die('Ошибка: ' . $E->getMessage() . PHP_EOL . 'Код ошибки: ' . $E->getCode());
+                die('Error: ' . $E->getMessage() . PHP_EOL . 'Error code: ' . $E->getCode());
             }
         }
     }

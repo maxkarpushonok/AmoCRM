dcf616b20da2bad97d07b89d61bf5b5032ddc02f
Max Karpushonok <max.karpushonok@gmail.com>
2020-03-02 23:28:23 +0300

    Modified to use the AmoCRM class

--- index.php
+++ index.php
@@ -1,77 +1,31 @@
 <?php
     ini_set('display_errors', 1);
 
+    session_start();
+
     include_once __DIR__ . '/vendor/autoload.php';
+    include_once 'AmoCRM.php'; //TODO auto load
 
-    //Массив с параметрами, которые нужно передать методом POST к API системы
-    $user = array(
-        'USER_LOGIN' => 'mazqazasd@outlook.com', //Ваш логин (электронная почта)
-        'USER_HASH' => '112f8e19ebf782451171efff26b1b071c65b2bbd', //Хэш для доступа к API (смотрите в профиле пользователя)
-    );
+    $amoCRM = new AmoCRM('mazqazasd@outlook.com', '112f8e19ebf782451171efff26b1b071c65b2bbd', 'mazqazasd');
 
-    $subdomain = 'mazqazasd'; //Наш аккаунт - поддомен
+    $authResult = $amoCRM->authorization();
 
-    //Формируем ссылку для запроса
-    $link = 'https://' . $subdomain . '.amocrm.ru/private/api/auth.php?type=json';
+    if ($authResult['auth']) {
+        $leadsResult = $amoCRM->getLeads();
+        $leadsId = array();
+        foreach ($leadsResult as $value)
+            $leadsId[] = $value['id'];
+        echo var_dump($leadsId).'<br>'; //TODO delete
 
-    /* Нам необходимо инициировать запрос к серверу. Воспользуемся библиотекой cURL (поставляется в составе PHP).
-       Вы также можете использовать и кроссплатформенную программу cURL, если вы не программируете на PHP. */
-    $curl = curl_init(); //Сохраняем дескриптор сеанса cURL
+        $tasksResult = $amoCRM->getTasks();
+        $tasksId = array();
+        foreach ($tasksResult as $value)
+            $tasksId[] = $value['element_id'];
+        echo var_dump($tasksId).'<br>'; //TODO delete
 
-    //Устанавливаем необходимые опции для сеанса cURL
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_USERAGENT, 'amoCRM-API-client/1.0');
-    curl_setopt($curl, CURLOPT_URL, $link);
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
-    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($user));
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-    curl_setopt($curl, CURLOPT_HEADER, false);
-    curl_setopt($curl, CURLOPT_COOKIEFILE, dirname
-        (__FILE__) . '/cookie.txt'); //PHP>5.3.6 dirname(__FILE__) -> __DIR__
-    curl_setopt($curl, CURLOPT_COOKIEJAR, dirname
-        (__FILE__) . '/cookie.txt'); //PHP>5.3.6 dirname(__FILE__) -> __DIR__
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+        $leadsIdWithoutTask = array_diff($leadsId, $tasksId);
+        echo var_dump($leadsIdWithoutTask).'<br>'; //TODO delete
 
-    $out = curl_exec($curl); //Инициируем запрос к API и сохраняем ответ в переменную
-    $code = curl_getinfo($curl, CURLINFO_HTTP_CODE); //Получим HTTP-код ответа сервера
-
-    curl_close($curl); //Завершаем сеанс cURL
-
-    /* Теперь мы можем обработать ответ, полученный от сервера. Это пример. Вы можете обработать данные своим способом. */
-    $code = (int) $code;
-    $errors = array(
-        301 => 'Moved permanently',
-        400 => 'Bad request',
-        401 => 'Unauthorized',
-        403 => 'Forbidden',
-        404 => 'Not found',
-        500 => 'Internal server error',
-        502 => 'Bad gateway',
-        503 => 'Service unavailable',
-    );
-
-    try
-    {
-        //Если код ответа не равен 200 или 204 - возвращаем сообщение об ошибке
-        if ($code != 200 && $code != 204) {
-            throw new Exception(isset($errors[$code]) ? $errors[$code] : 'Undescribed error', $code);
-        }
-
-    } catch (Exception $E) {
-        die('Ошибка: ' . $E->getMessage() . PHP_EOL . 'Код ошибки: ' . $E->getCode());
     }
 
-    /*
-    Данные получаем в формате JSON, поэтому, для получения читаемых данных,
-    нам придётся перевести ответ в формат, понятный PHP
-     */
-    $Response = json_decode($out, true);
-    $Response = $Response['response'];
-
-    //Флаг авторизации доступен в свойстве "auth"
-    if (isset($Response['auth'])) {
-
-        echo 'Авторизация прошла успешно';
-    } else
-        echo 'Авторизация не удалась';
+    $amoCRM->closeCurl();
